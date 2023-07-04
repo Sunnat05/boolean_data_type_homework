df395335d17cf5357a22f55f7fa570deb2d490e1
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 00:55:53 +0500

    nth

--- bool10.py
+++ bool10.py
@@ -1,4 +1,5 @@
 def main(a):
+    ((a)**(1/2))%1==0 and a>=0
     """check that the number "a" is a perfect square.
     Args:
         a: int
@@ -6,4 +7,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return ((a)**(1/2))%1==0 and a>=0
+print(main(16))
